bb9e9291572307448e3d3946109813074d98d272
유수현 <rhan0871@naver.com>
2022-01-15 01:13:23 +0900

    Updates

--- precache-manifest.6d0c149a23b9c63bb6f7d8c2b7070d93.js
+++ precache-manifest.6d0c149a23b9c63bb6f7d8c2b7070d93.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6833b596da2e073dee4b41d30a516b82",
+    "revision": "4c31528223cb1487a0924fd62565beab",
     "url": "/every-gp/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/every-gp/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "a4bcaf8b73ed9dfc84e5",
+    "revision": "f580618c2bf668210727",
     "url": "/every-gp/static/css/main.ffe27495.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/every-gp/static/js/2.edc3b906.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a4bcaf8b73ed9dfc84e5",
-    "url": "/every-gp/static/js/main.68cdcfd7.chunk.js"
+    "revision": "f580618c2bf668210727",
+    "url": "/every-gp/static/js/main.cafb394b.chunk.js"
   },
   {
     "revision": "b8d5ef450f47848e2cf3",
