c2a85dca3bdfac308beedfceeb89f3b4284984bf
유수현 <rhan0871@naver.com>
2022-02-18 16:03:06 +0900

    '-'

--- src/App.jsx
+++ src/App.jsx
@@ -33,8 +33,8 @@ function App({ userRole, isLogin, login, logout }) {
     }
   }, []);
   return (
-    <div className={browserWidth<776?"Hide":"App"} >
-      {browserWidth<776?(
+    <div className={browserWidth<879?"Hide":"App"} >
+      {browserWidth<879?(
         <div id="lowWidthAlert">
           <h2>브라우저 크기가 너무 작습니다 :(</h2>
           <b>본 서비스는 태블릿과 PC화면 크기에 최적화된 서비스입니다. 브라우저 크기를 키워주세요 😀</b>
